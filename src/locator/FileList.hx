bbcaf23bb4b6202edc690869be15ca24cc3bda26
FAL <falworks.contact@gmail.com>
2020-06-15 15:27:52 +0900

    add FileList.fileNames()

--- src/locator/FileList.hx
+++ src/locator/FileList.hx
@@ -20,11 +20,17 @@ abstract FileList(Data) from Data to Data {
 		return this;
 
 	/**
+		@return New list of file names without directory paths.
+	**/
+	public inline function fileNames(): Array<String>
+		return this.map(file -> file.getName());
+
+	/**
 		Copies all files in `this` list to `destination` with the same file names.
 		Overwrites destination files if they already exist.
 		@param prepareDirectory If `true`, creates the destination directory if absent.
 	**/
-	public extern inline function copyTo(
+	public inline function copyTo(
 		destinationPath: DirectoryPath,
 		prepareDirectory = true
 	): Void {
