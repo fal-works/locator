61250ca6230dca48c09d05495bea4d58674dca96
FAL <falworks.contact@gmail.com>
2022-04-08 18:01:29 +0900

    feat: add DirectoryRef.traverse()

--- src/locator/DirectoryRef.hx
+++ src/locator/DirectoryRef.hx
@@ -119,6 +119,31 @@ abstract DirectoryRef(DirectoryPath) {
 	}
 
 	/**
+		Traverses all files and directories recursively starting from `this`.
+		@param visit Function to be called for each file or directory (including `this`).
+			The `depth` argument is the traversal depth level,
+			which is `0` for `this`, `1` for the children of `this` and so on.
+			See `TraversalInstruction` for the return value.
+		@return `FileOrDirectoryRef` if found any.
+	**/
+	public function traverse(
+		visit: (
+			name: String,
+			isDirectory: Bool,
+			depth: Int
+		) -> TraversalInstruction
+	): Maybe<FileOrDirectoryRef> {
+		return switch visit(this.getName(), true, 0) {
+			case Continue:
+				traverseRecursive(visit, 1);
+			case SkipChildren:
+				Maybe.none();
+			case Found:
+				Maybe.from(FileOrDirectoryRef.fromDirectory(DirectoryRef.fromPathUnsafe(this)));
+		}
+	}
+
+	/**
 		Copies `this` directory to `destinationPath` with all of its contents (recursively).
 		Overwrites the destination files if it already exist.
 		@param destinationPath The destination path.
@@ -157,4 +182,61 @@ abstract DirectoryRef(DirectoryPath) {
 
 	extern inline function get_path(): DirectoryPath
 		return this;
+
+	function traverseRecursive(
+		visit: (
+			name: String,
+			isDirectory: Bool,
+			depth: Int
+		) -> TraversalInstruction,
+		depth: Int
+	): Maybe<FileOrDirectoryRef> {
+		for (name in FileSystem.readDirectory(this)) {
+			final path: PathString = this + name;
+
+			if (FileSystem.isDirectory(path)) {
+				final dir = DirectoryRef.fromPathUnsafe(path);
+				switch visit(name, true, depth) {
+					case Continue:
+						return dir.traverseRecursive(visit, depth + 1);
+					case SkipChildren:
+					case Found:
+						return Maybe.from(FileOrDirectoryRef.fromDirectory(dir));
+				}
+			} else {
+				final file = FileRef.fromPathUnsafe(path);
+				switch visit(name, false, depth) {
+					case Continue:
+					case SkipChildren:
+					case Found:
+						return Maybe.from(FileOrDirectoryRef.fromFile(file));
+				}
+			}
+		}
+
+		return Maybe.none();
+	}
+}
+
+/**
+	Value for controlling a file traversal process.
+**/
+enum abstract TraversalInstruction(Int) {
+	/**
+		Continues the traversal.
+		Children files/directories will be traversed as well (if there are any).
+	**/
+	final Continue;
+
+	/**
+		Continues the traversal.
+		Children files/directories will ont be traversed (if there are any).
+		If there are no children, the traversal will continue in the same way as `Continue`.
+	**/
+	final SkipChildren;
+
+	/**
+		Stops the traversal and returns the found file/directory.
+	**/
+	final Found;
 }
